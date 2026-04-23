655b51b7fcb4b82bcce7ef4c485f3ce1fff5c703
Waize-95 <veezobutt@gmail.com>
2026-04-23 05:45:44 +0500

    complete_command.cpp

--- raftkv/src/command.cpp
+++ raftkv/src/command.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 
@@ -11,11 +12,33 @@ struct Command{
     string value;
 };
 
-vector<u_int8_t> Serialize(u_int8_t op,u_int16_t key_l,u_int8_t val_l, string key,
-string _value){
-
+vector<u_int8_t> Serialize(u_int8_t op,u_int16_t key_l,u_int16_t val_l, const string& _key,
+const string& _value){
     vector<u_int8_t> result;
-    
+    result.reserve(5+key_l+val_l);
 
+    result.push_back(op);
+    u_int8_t key_high=static_cast<u_int8_t>((key_l>>8)&0xFF);
+    result.push_back(key_high);
+    u_int8_t key_low=static_cast<u_int8_t>(key_l&0xFF);
+    result.push_back(key_low);
+    u_int8_t val_high=static_cast<u_int8_t>((val_l>>8)&0xFF);
+    result.push_back(val_high);
+    u_int8_t val_low=static_cast<u_int8_t>(val_l&0xFF);
+    result.push_back(val_low);
+    result.insert(result.end(),_key.begin(),_key.end());
+    result.insert(result.end(),_value.begin(),_value.end());
+    return result;
+}
 
+Command Deserialize(const vector<u_int8_t>& result){
+    Command cmd;
+    cmd.operation=result[0];
+    u_int16_t key_len1=static_cast<u_int16_t>((result[1]<<8)|result[2]);
+    u_int16_t val_len1=static_cast<u_int16_t>((result[3]<<8)|result[4]);
+    cmd.key_len=key_len1;
+    cmd.value_len=val_len1;
+    cmd.key=string(result.begin()+5,result.begin()+5+key_len1);
+    cmd.value=string(result.begin()+5+key_len1,result.begin()+5+key_len1+val_len1);
+    return cmd;
 }
